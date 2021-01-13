233def2332b15f8ecc81ecc9c36eb4ac140e4166
midhun000 <46200426+midhun000@users.noreply.github.com>
2021-01-13 08:45:26 +0530

    Initial commit

---
+++ src/com/luv2code/springdemo/TrackCoach.java
@@ -0,0 +1,32 @@
+package com.luv2code.springdemo;
+
+public class TrackCoach implements Coach {
+	
+	private FortuneService fortuneService;
+		
+	public TrackCoach(FortuneService fortuneService) {
+		super();
+		this.fortuneService = fortuneService;
+	}
+	
+	
+
+	public TrackCoach() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+	@Override
+	public String getDailyWorkout() {
+		return "Run a hard 5K";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		
+		return "Just Do It; "+fortuneService.getFortune(); 
+	}
+
+}
